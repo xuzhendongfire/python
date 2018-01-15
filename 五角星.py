606a01a8f523125ba0b516ed94e5e52cd061418b
许振东 <839638952@qq.com>
2018-01-15 15:04:31 +0800

    1
    第一次提交

---
+++ 五角星.py
@@ -0,0 +1,21 @@
+import turtle
+
+p = turtle.Turtle()
+p.speed(1)
+p.pensize(2)
+p.color("black","yellow")
+
+q = turtle.Turtle()
+q.speed(2)
+q.pensize(2)
+q.color("black","yellow")
+
+p.begin_fill()
+q.begin_fill()
+for i in range(5):
+    p.forward(200)
+    q.forward(-200)
+    p.right(144)
+    q.left(144)
+p.end_fill()
+q.end_fill()
